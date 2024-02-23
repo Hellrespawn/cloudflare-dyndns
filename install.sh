f8968ae0cdec9ab6facbfeb1f2d36ca4dabad2f0
Stef Korporaal <stef@skorporaal.nl>
2024-02-23 21:22:15 +0100

    Create dir in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cargo build --release
-sudo cp target/release/cloudflare-dyndns /opt/cloudflare-dyndns
-sudo cp cloudflare-dyndns.service /etc/systemd/system
-sudo cp cloudflare-dyndns.timer /etc/systemd/system
+sudo mkdir -p /opt/cloudflare-dyndns
+sudo cp target/release/cloudflare-dyndns /opt/cloudflare-dyndns/
+sudo cp cloudflare-dyndns.service /etc/systemd/system/
+sudo cp cloudflare-dyndns.timer /etc/systemd/system/
 sudo systemctl daemon-reload
